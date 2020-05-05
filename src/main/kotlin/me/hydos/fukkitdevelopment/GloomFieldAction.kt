8420f2b180559d32619b692c00327b326eeb2411
hYdos <haydenv06@gmail.com>
2020-05-06 06:49:09 +1000

    gradle likes stealing ram

---
+++ src/main/kotlin/me/hydos/fukkitdevelopment/GloomFieldAction.kt
@@ -0,0 +1,18 @@
+package me.hydos.fukkitdevelopment
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GloomFieldAction : AnAction() {
+    /**
+     * Implement this method to provide your action handler.
+     *
+     * @param e Carries information on the invocation place
+     */
+    override fun actionPerformed(e: AnActionEvent) {
+
+
+
+    }
+
+}
