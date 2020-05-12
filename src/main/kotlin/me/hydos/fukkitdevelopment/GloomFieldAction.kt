952aa7dbf361ae921e12d2a5f201bd643f7b6d79
hYdos <haydenv06@gmail.com>
2020-05-12 19:06:43 +1000

    send help

--- src/main/kotlin/me/hydos/fukkitdevelopment/GloomFieldAction.kt
+++ src/main/kotlin/me/hydos/fukkitdevelopment/GloomFieldAction.kt
@@ -1,30 +1,47 @@
-package me.hydos.fukkitdevelopment
+package me.hydos.fukkitdevelopment;
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import io.github.fukkitmc.gloom.DefinitionSerializer
-import java.awt.event.ActionEvent
-import java.io.File
-import javax.swing.JButton
-
+import com.intellij.openapi.actionSystem.CommonDataKeys.CARET
+import com.intellij.openapi.actionSystem.CommonDataKeys.PSI_FILE
+import com.intellij.openapi.wm.WindowManager
+import com.intellij.psi.*
+import com.intellij.psi.util.elementType
 
 class GloomFieldAction : AnAction() {
-    /**
-     * Implement this method to provide your action handler.
-     *
-     * @param e Carries information on the invocation place
-     */
+
     override fun actionPerformed(e: AnActionEvent) {
+        print(1)
+        val project = e.project ?: return
+        val file = e.getData(PSI_FILE) ?: return
+        val caret = e.getData(CARET) ?: return
 
-        val definitions = DefinitionSerializer.fromString(File("E:/dev/fabric/mods/LegacyFukkit/definitions/fields.json").readText());
-        val testButton = JButton()
-        testButton.addActionListener { actionEvent: ActionEvent? ->
-            if (FieldTextBox().showAndGet()) {
+        val element = file.findElementAt(caret.offset) ?: return
+        val member = element.findReferencedMember() ?: return
 
-            }
+        val targetReference = when (member) {
+            is PsiMethod -> member.qualifiedMemberReference
+            is PsiField -> member.qualifiedMemberReference
+            else -> return
         }
 
+        WindowManager.getInstance().getStatusBar(project).info = "Adding $targetReference to Gloom fields"
+        println(file.parent)
+        println(file)
 
+        val methodName = element.text;
+        val methodDescriptor = toString(targetReference);
+
+        println(methodName)
+        println(methodDescriptor)
+    }
+
+    private fun toString(reference: MemberReference): String {
+        val builder = StringBuilder()
+        if (reference.owner != null) {
+            builder.append('L').append(reference.owner.replace('.', '/')).append(';')
+        }
+        return builder.toString()
     }
 
 }
